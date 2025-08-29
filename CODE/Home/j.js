fd13ded211600ba31480b4d48aca35bd0d3e37b6
Joy Goswami <joygoswaminiloy2023@gmail.com>
2025-08-30 00:37:35 +0600

    add; homepage code file(HTML Tailwind-CSS,JS) , image, icon

---
+++ CODE/Home/j.js
@@ -0,0 +1,25 @@
+const btn = document.getElementById('menu-btn');
+    const menu = document.getElementById('mobile-menu');
+
+    btn.addEventListener('click', () => {
+      menu.classList.toggle('hidden');
+    });
+
+    const dropdownBtn = document.getElementById('dropdown-btn');
+    const dropdownMenu = document.getElementById('dropdown-menu');
+
+    dropdownBtn.addEventListener('click', () => {
+      dropdownMenu.classList.toggle('hidden');
+    });
+
+
+      document.querySelectorAll(".faq-item").forEach(item => {
+    item.addEventListener("click", () => {
+      const content = item.querySelector("p");
+      const icon = item.querySelector("i");
+
+      content.classList.toggle("hidden");
+      icon.classList.toggle("fa-plus");
+      icon.classList.toggle("fa-minus");
+    });
+  });
