3c39a7d271541c94b157fe1c7e142e2827404cd4
Joy Goswami <joygoswaminiloy2023@gmail.com>
2025-10-13 00:01:34 +0600

    add clinic donation login rescue

---
+++ CODE/js/j.js
@@ -0,0 +1,48 @@
+
+// mobile toggle navbar
+const btn = document.getElementById('menu-btn');
+    const menu = document.getElementById('mobile-menu');
+
+    btn.addEventListener('click', () => {
+      menu.classList.toggle('hidden');
+    });
+
+    const dropdownBtn = document.getElementById('dropdown-btn');
+    const dropdownMenu = document.getElementById('dropdown-menu');
+
+    dropdownBtn.addEventListener('click', () => {
+      dropdownMenu.classList.toggle('hidden');
+    });
+
+
+      document.querySelectorAll(".faq-item").forEach(item => {
+    item.addEventListener("click", () => {
+      const content = item.querySelector("p");
+      const icon = item.querySelector("i");
+
+      content.classList.toggle("hidden");
+      icon.classList.toggle("fa-plus");
+      icon.classList.toggle("fa-minus");
+    });
+  });
+
+
+
+  // rescue dropdownBtn
+
+  const menuBtn = document.getElementById("menu-btn");
+  const mobileMenu = document.getElementById("mobile-menu");
+  menuBtn.addEventListener("click", () => {
+    mobileMenu.classList.toggle("hidden");
+  });
+
+
+  //clinic counter
+      let counter = 0;
+    const button = document.getElementById('clickBtn');
+    const countDisplay = document.getElementById('count');
+
+    button.addEventListener('click', () => {
+      counter++; 
+      countDisplay.textContent = counter; 
+    });
