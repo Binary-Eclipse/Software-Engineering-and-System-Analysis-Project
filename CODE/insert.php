a52b5b2b16abdf49f807d9d312bca92ddfe4525b
Joy Goswami <joygoswaminiloy2023@gmail.com>
2025-10-16 10:42:27 +0600

    commit: replacing the code folder with the fixed file directory

---
+++ CODE/insert.php
@@ -0,0 +1,34 @@
+<?php
+$conn = new mysqli("localhost", "root", "", "rescue");
+
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$name = $_POST['name'] ?? '';
+$phn  = $_POST['phn'] ?? '';
+$loc  = $_POST['loc'] ?? '';
+$type= $_POST['animal_t'] ?? '';
+$description=$_POST['des'] ?? '';
+
+if (!empty($name) && !empty($phn) && !empty($loc) && !empty($type) && !empty($description)) {
+  $stmt = $conn->prepare("INSERT INTO res (name, phn, loc, type, des) VALUES (?, ?, ?, ?, ?)");
+  $stmt->bind_param("sssss", $name, $phn, $loc, $type, $description);
+  $stmt->execute();
+  $stmt->close();
+  
+  // Show alert and redirect using JS
+  echo '<script>
+          alert("Info Inserted Successfully");
+          window.location.href = "rescue.php";
+        </script>';
+  exit();
+} else {
+  echo '<script>
+          alert(" Missing some data (location or input fields)");
+          window.location.href = "rescue.php";
+        </script>';
+}
+
+$conn->close();
+?>
