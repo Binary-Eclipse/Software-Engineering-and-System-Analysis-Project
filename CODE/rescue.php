bd95b867b4c20929a930a9cfdcaebe48a8149ca2
Joy Goswami <joygoswaminiloy2023@gmail.com>
2025-10-17 23:04:12 +0600

    Updated rescue

--- CODE/rescue.php
+++ CODE/rescue.php
@@ -8,24 +8,21 @@ $loc  = $_POST['loc'] ?? '';
 $type= $_POST['animal_t'] ?? '';
 $description=$_POST['des'] ?? '';
 
-if (!empty($name) && !empty($phn) && !empty($loc) && !empty($type) && !empty($description)) {
+
+
   $stmt = $conn->prepare("INSERT INTO res (name, phn, loc, type, des) VALUES (?, ?, ?, ?, ?)");
   $stmt->bind_param("sssss", $name, $phn, $loc, $type, $description);
   $stmt->execute();
   $stmt->close();
   
   // Show alert and redirect using JS
+
   echo '<script>
           alert("Info Inserted Successfully");
           window.location.href = "rescue.html";
         </script>';
-  exit();
-} else {
-  echo '<script>
-          alert(" Missing some data (location or input fields)");
-          window.location.href = "rescue.html";
-        </script>';
-}
+exit();
+
 
 $conn->close();
 ?>
